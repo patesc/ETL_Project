809f1974bf0f91edd8368de762705c0e9e2a9e22
pat <pat.lim.escalante@gmail.com>
2018-11-19 20:03:54 -0500

    updated schema

--- schema.sql
+++ schema.sql
@@ -4,7 +4,6 @@ USE presidents_data;
 CREATE TABLE pres_data (
 id INT PRIMARY KEY,
 president TEXT,
-party TEXT,
 annual_gnp_growth_pct INT,
 annual_debt_increase_pct INT,
 ufo_sightings INT,
@@ -15,10 +14,15 @@ daily_cig_sale_per_adult INT
 CREATE TABLE pres_agg (
 id INT PRIMARY KEY,
 president TEXT,
-party TEXT,
 annual_gnp_growth_pct INT,
 annual_debt_increase_pct INT,
 ufo_sightings INT,
 year INT,
 daily_cig_sale_per_adult INT
 );
+
+select * from pres_data;
+
+show tables;
+
+drop table press_data
